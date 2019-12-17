6d66a0847951222ab3539c0fb8bec9456f6354de
cmdtvt <tvtuusa@gmail.com>
2019-12-18 00:39:46 +0200

    Smoothing and Free the mouse
    Smoothed out the movement keys so doesn't cut the movement everytime key is released.
    Added also a way to free the mouse from the middle of screen by holding left alt. This is made so using GUI buttons is actually plausible.

--- cameraController.py
+++ cameraController.py
@@ -11,6 +11,7 @@ class FPScamera():
 
         
         self.debug = False
+        self.mouseInUse = True #### If false releases the mouse so it can be moved freely
         self.cameraSpeed = 30
         self.testlockx = 0
         self.testlocky = 0
@@ -22,14 +23,15 @@ class FPScamera():
         #### All allowed keys are stored in dictionary                               ####
         #### If you need to add things to FPS camera you must first add it to keyMap ####
         #################################################################################
-        self.keyMap = {"w" : False, "s" : False, "a" : False, "d" : False,}
+        self.keyMap = {"w" : False, "s" : False, "a" : False, "d" : False,"lalt": False}
         self.map = map
 
-        print(self.map.get_mapped_button("w"))
+        print(self.map.get_mapped_button("lalt"))
         self.w_button = self.map.get_mapped_button("w")
         self.a_button = self.map.get_mapped_button("a")
         self.s_button = self.map.get_mapped_button("s")
         self.d_button = self.map.get_mapped_button("d")
+        self.lalt_button = self.map.get_mapped_button("lalt")
 
         #### Because of some reason this is a camera object?!? ####
 
@@ -44,7 +46,7 @@ class FPScamera():
                 self.print("Button: "+str(self.w_button)+" is pressed")
 
 
-        #### Movement cheks if key is pressed move else dont move ####
+        #### Toggles True or False if key is pressed. this is saved into self.keyMap ####
         #### W ####
         if base.mouseWatcherNode.is_button_down(self.w_button):
             self.setKey("w", True)
@@ -68,7 +70,13 @@ class FPScamera():
             self.setKey("d", True)
         else:
             self.setKey("d", False)
-        #print("controls run!")
+
+        #### LEFT ALT ####
+        if base.mouseWatcherNode.is_button_down(self.lalt_button):
+            self.setKey("lalt", True)
+        else:
+            self.setKey("lalt", False)
+            
             
     def print(self,text):
         print("####FPScamera: "+str(text))
@@ -99,43 +107,58 @@ class FPScamera():
         self.windowXsize = base.win.getXSize()
         self.windowYSize = base.win.getYSize()
 
+
+
+        if (self.keyMap["lalt"] == True):
+            self.mouseInUse = False
+        else:
+            base.win.movePointer(0, int(self.windowXsize/2) , int(self.windowYSize/2))
+            self.mouseInUse = True
+            
+
         
         #### This stuff is executed if mouse is on screen i think ####
         if(base.mouseWatcherNode.hasMouse() == True):
             mpos = base.mouseWatcherNode.getMouse()
-            base.win.movePointer(0, int(self.windowXsize/2) , int(self.windowYSize/2))
+            base.mouseWatcherNode.set_effects
 
-            #### Checks if camera is moving left or right ####
-            if mpos[0] <= 0.0000 or mpos[0] >= 0.0000:
-                self.testlockx -= (mpos[0]*self.sensitivity)
-                base.camera.setH(self.testlockx)
 
-            #### Checks up and down movement ####
-            if mpos[1] <= 0.0000 or mpos[1] >= 0.0000:
-                if self.debug == True:
-                    print("Going Down")
-                self.testlocky += (mpos[1]*self.sensitivity)
-                base.camera.setP(self.testlocky)
+            if self.mouseInUse == True:
+                base.win.movePointer(0, int(self.windowXsize/2) , int(self.windowYSize/2))
+                #### Checks if camera is moving left or right ####
+                if mpos[0] <= 0.0000 or mpos[0] >= 0.0000:
+                    self.testlockx -= (mpos[0]*self.sensitivity)
+                    base.camera.setH(self.testlockx)
+
+                #### Checks up and down movement ####
+                if mpos[1] <= 0 or mpos[1] >= 0:
+                    if self.debug == True:
+                        print("Going Down")
+                    self.testlocky += (mpos[1]*self.sensitivity)
+                    base.camera.setP(self.testlocky)
+            
             		
         if(self.keyMap["w"] == True):
             base.camera.setY(base.camera, self.cameraSpeed * dt)
             if self.debug == True:
                 print("camera moving forward")
         
-        elif(self.keyMap["s"] == True):
+        if(self.keyMap["s"] == True):
             base.camera.setY(base.camera, -self.cameraSpeed * dt)
             if self.debug == True:
                 print("camera moving backwards")
         
-        elif(self.keyMap["a"] == True):
+        if(self.keyMap["a"] == True):
             base.camera.setX(base.camera, -self.cameraSpeed * dt)
             if self.debug == True:
                 print("camera moving left")
         
-        elif(self.keyMap["d"] == True):
+        if(self.keyMap["d"] == True):
             base.camera.setX(base.camera, self.cameraSpeed * dt)
             if self.debug == True:
                 print("camera moving right")
+
+        
 
     def toggleDebug(self,):
         if self.debug == False:
@@ -143,6 +166,15 @@ class FPScamera():
         else:
             self.debug = False
         print("Toggled debug to: "+str(self.debug))
+
+    def freeMouse(self,):
+        if self.mouseInUse == False:
+            self.mouseInUse = True
+            print("Mouse is free.")
+        else:
+            self.mouseInUse = False
+            print("Mouse Locked.")
+        
         
 
 
